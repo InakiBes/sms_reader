6814bf64907d046a44a32d0ca643a4e5e4075f18
InakiBes <inakibes2@gmail.com>
2023-09-14 19:50:50 +0200

    feat: basic structure to read incoming sms

--- app/src/main/java/com/religada/smsreader/MainActivity.kt
+++ app/src/main/java/com/religada/smsreader/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.religada.smsreader
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.Telephony
+import androidx.appcompat.app.AppCompatActivity
+import com.religada.smsreader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            listenIncomingSms()
+        }
+    }
+
+    private fun listenIncomingSms() {
+        if (checkSelfPermission(android.Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED
+            || checkSelfPermission(android.Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED
+            || checkSelfPermission(android.Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(
+                    android.Manifest.permission.RECEIVE_SMS,
+                    android.Manifest.permission.SEND_SMS,
+                    android.Manifest.permission.READ_SMS
+                ), PackageManager.PERMISSION_GRANTED
+            )
+        }
+
+
+        val br = object : BroadcastReceiver() {
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                for (sms in Telephony.Sms.Intents.getMessagesFromIntent(
+                    p1
+                )) {
+                    val smsSender = sms.originatingAddress // This is the number that sends the sms
+                    val smsMessageBody = sms.displayMessageBody
+                    binding.tvSmsViewer.text = smsMessageBody
+                }
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            registerReceiver(
+                br,
+                IntentFilter("android.provider.Telephony.SMS_RECEIVED"),
+                RECEIVER_EXPORTED
+            )
+        }
     }
 }
